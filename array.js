7156d301d0655fccd3e11786ce4da42a7b2e440b
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2023-01-29 14:41:01 -0300

    adicionando uma nova variavel em branch secudaria

--- array.js
+++ array.js
@@ -3,6 +3,7 @@
 let mochila=['lapis','apontador','caderno'];
    
   let dt=new Date();
-  hora=dt.getHours();
-  minutos=dt.getMinutes();
+  let hora=dt.getHours();
+  let minutos=dt.getMinutes();
+  let segundos=dt.getSeconds(); //adicionando uma nova variável numa branch secundaria. 
   console.log(`hora :${hora}:${minutos}`);
