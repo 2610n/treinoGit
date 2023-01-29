cf601afabcd7971c6df8c2447ddc8cef2b3abc51
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2023-01-29 14:16:15 -0300

    adicionando os minutos

--- array.js
+++ array.js
@@ -4,4 +4,5 @@ let mochila=['lapis','apontador','caderno'];
    
   let dt=new Date();
   hora=dt.getHours();
-  console.log(`hora :${hora}`);
+  minutos=dt.getMinutes();
+  console.log(`hora :${hora}:${minutos}`);
